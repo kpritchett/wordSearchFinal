b19f5aa6402960faded657ada252f8578e1614ac
mphipps <student@bluc02mg32jfh51.local>
2016-05-12 10:16:24 -0500

    Made some changes dud

--- Word Search 2/WordSearchClass.swift
+++ Word Search 2/WordSearchClass.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WordSearchClass: NSObject
 {
-    var lettersArray: [[String]] = []
+    var wordSearch: [[String]] = []
     var wordsArray: [Word] = []
     var wordsDirections: [Int] = []
     
@@ -38,50 +38,54 @@ class WordSearchClass: NSObject
         
         // Determines size of word search based on How many words are imputed
         sortWordsArray()
-        var width = 9
-        var height = 16
+        let column = 14
+        let row = 21
         
-        if wordsArray.count > 6
+        for var r = 0; r < row; r++
         {
-            width = 14
-            height = 21
-        }
-        for var r = 0; r < width; r++
-        {
-            var words: [String] = []
-            for var c = 0; c < height; c++
+            var blankSpaces: [String] = []
+            for var c = 0; c < column; c++
             {
-                let letterPosition = Int(arc4random_uniform(26))
-                let myRange = Range<String.Index>(start: alphabet.startIndex.advancedBy(letterPosition), end: alphabet.startIndex.advancedBy(letterPosition + 1))
-                words.append(alphabet.substringWithRange(myRange))
+                //let letterPosition = Int(arc4random_uniform(26))
+                //let myRange = Range<String.Index>(start: alphabet.startIndex.advancedBy(letterPosition), end: alphabet.startIndex.advancedBy(letterPosition + 1))
+                //words.append(alphabet.substringWithRange(myRange))
+                blankSpaces.append("")
             }
-            lettersArray.append(words)
+            wordSearch.append(blankSpaces)
         }
+        for var i = 0; i < wordsArray.count; i++
+        {
+            var yes = false
+            while(yes == false)
+            {
+                switch abs(wordsDirections[i])
+                {
+                case 1:
+                    insertWordHorizontal(wordsArray[i].getWord(), Row: Int(arc4random_uniform(14)), Col: Int(arc4random_uniform(21)))
+                default:
+                    print("didnt work")
+                }
+            }
+        }
+        
     }
-    func insertWordHorizontal(Word: String, Row: Int, var Col: Int, Dir: Int)
+    
+    func insertWordHorizontal(Word: String, Row: Int, var Col: Int)
     {
-        if(Dir == 1)
+
+        for var i = 0; i < Word.characters.count; i++
         {
-            for var i = 0; i < Word.characters.count; i++
-            {
-                let myRange = Range<String.Index>(start: Word.startIndex.advancedBy(i), end: Word.startIndex.advancedBy(i + 1))
-                lettersArray[Row][Col] = Word.substringWithRange(myRange)
-                Col++
-            }
+            let myRange = Range<String.Index>(start: Word.startIndex.advancedBy(i), end: Word.startIndex.advancedBy(i + 1))
+            wordSearch[Row][Col] = Word.substringWithRange(myRange)
+            Col++
         }
-        else
-        {
-            for var i = 0; i < Word.characters.count; i++
-            {
-                let myRange = Range<String.Index>(start: Word.startIndex.advancedBy(i), end: Word.startIndex.advancedBy(i + 1))
-                lettersArray[Row][Col] = Word.substringWithRange(myRange)
-                Col--
-            }
-        }
+        
     }
+    
     func insertVerticalWord(Word: String, Row: Int, Col: Int)
     {
     }
+    
     func sortWordsArray()
     {
         for var i = 0; i < wordsArray.count - 1; i++
@@ -94,5 +98,21 @@ class WordSearchClass: NSObject
                 i = -1
             }
         }
+    }
+    
+    func makeWordBackwards(Word: String) -> String
+    {
+        var wordBackWardsArray: [String] = []
+        for var i = 0; i < Word.characters.count; i++
+        {
+            let myRange = Range<String.Index>(start: Word.startIndex.advancedBy(i), end: Word.startIndex.advancedBy(i + 1))
+            wordBackWardsArray.append(Word.substringWithRange(myRange))
+        }
+        var yes = ""
+        for var i = Word.characters.count - 1; i >= 0; i--
+        {
+            yes += wordBackWardsArray[i]
+        }
+        return yes
     }
 }
